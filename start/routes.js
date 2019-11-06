5dade00041826b975c472abbe3d9472e3c107a1b
ArielBailon <arielbailonn@outlook.es>
2019-11-06 17:37:09 -0500

    crud actualizado a respuestas chatbot

--- start/routes.js
+++ start/routes.js
@@ -17,11 +17,11 @@
 const Route = use('Route')
 
 Route.group(() =>{
-  Route.get('tasks', 'TaskController.index')
-  Route.get('task/:id', 'TaskController.show')
-  Route.post('task', 'TaskController.store')
-  Route.put('task/:id', 'TaskController.update')
-  Route.delete('task/:id', 'TaskController.delete')
+  Route.get('chatbot', 'RespuestaChatController.indice')
+  Route.get('chatbot/:id', 'RespuestaChatController.mostrar')
+  Route.post('chatbot', 'RespuestaChatController.guardar')
+  Route.put('chatbot/:id', 'RespuestaChatController.actualizar')
+  Route.delete('chatbot/:id', 'RespuestaChatController.eliminar')
 
 }).prefix('api')
 
