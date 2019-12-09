c2049b6a888567800f3672aead126dd7c85bdf66
ArielBailon <arielbailonn@outlook.es>
2019-12-09 11:54:26 -0500

    subir chat

--- public/chat.js
+++ public/chat.js
@@ -1,42 +1,14 @@
 let ws = null
 
-<<<<<<< HEAD
 
 $(function () {
   // Only connect when username is available
   if (window.username) {
     startChat()
-=======
-// $(function () {
-//  Empezar el chat
-//     startChat()
-// })
-
-function crearChat() {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://127.0.0.1:3333/crearchat', true);
-
-  xhr.onload = function() {
-    if (this.status == 200) {
-      // console.log(this.responseText);
-      // var id = this.responseText;
-      subscribeToChannel(this.responseText)
-    }
->>>>>>> arielbranch
   }
 
-<<<<<<< HEAD
 function startChat() {
-  ws = adonis.Ws().connect()      
-=======
-  xhr.send()
-
-}
-
-function startChat(chatId) {
   ws = adonis.Ws().connect()
-
->>>>>>> arielbranch
   ws.on('open', () => {
     $('.connection-status').addClass('connected')
     console.log(chatId)
@@ -57,13 +29,8 @@ function subscribeToChannel(id) {
   })
 
   chat.on('message', (message) => {
-<<<<<<< HEAD
-    $('.messages').append(`      
-      <p class="userEnteredText">${message.body}</p><div class="clearfix"></div><p class="botResult">${message.body}</p><div class="clearfix"></div>
-=======
     $('.messages').append(`
-      <div class="message"><h4> Usuario </h4> <p> ${message.body} </p> </div>
->>>>>>> arielbranch
+      <p class="userEnteredText">${message.body}</p><div class="clearfix"></div><p class="botResult">${message.body}</p><div class="clearfix"></div>
     `)
   })
 }
